4ff72663c9f4a06c605793269eafe285051be371
Henrique Abreu <abreu-semperfi@icloud.com>
2020-07-04 17:54:25 +0100

    added custom cell to tableView

---
+++ MemeGenerator/MemeTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeTableCell.swift
+//  MemeGenerator
+//
+//  Created by Henrique Abreu on 04/07/2020.
+//  Copyright © 2020 Henrique Abreu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableCell: UITableViewCell{
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topText: UILabel!
+    @IBOutlet weak var bottomText: UILabel!
+    
+}
